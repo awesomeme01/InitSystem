26944f2dd6e08af18b811a779b45d51bd9cd3d2d
awesomeme01 <shabdan.abjaparov.official@gmail.com>
2020-10-15 13:45:10 +0600

    prep4

--- src/main/java/com/example/demo/model/Attendance.java
+++ src/main/java/com/example/demo/model/Attendance.java
@@ -1,63 +1,63 @@
-package com.example.demo.model;
-
-import javax.persistence.*;
-import java.time.LocalDateTime;
-
-@Entity
-@Table(name = "attendance_init_1")
-public class Attendance {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-
-    @ManyToOne
-    @JoinColumn(name = "user_id", nullable = false)
-    private User user;
-
-    @Column(name = "date_time", nullable = false)
-    private LocalDateTime localDateTime;
-
-    @Column(name = "state", nullable = false)
-    private int state;
-
-    public Attendance(){
-        this.localDateTime = LocalDateTime.now();
-    }
-    public Attendance(User user, int state) {
-        this.user = user;
-        this.localDateTime = LocalDateTime.now();
-        this.state = state;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public LocalDateTime getLocalDateTime() {
-        return localDateTime;
-    }
-
-    public void setLocalDateTime(LocalDateTime localDateTime) {
-        this.localDateTime = localDateTime;
-    }
-
-    public int getState() {
-        return state;
-    }
-
-    public void setState(int state) {
-        this.state = state;
-    }
-}
+//package com.example.demo.model;
+//
+//import javax.persistence.*;
+//import java.time.LocalDateTime;
+//
+//@Entity
+//@Table(name = "attendance_init_1")
+//public class Attendance {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    private Long id;
+//
+//    @ManyToOne
+//    @JoinColumn(name = "user_id", nullable = false)
+//    private User user;
+//
+//    @Column(name = "date_time", nullable = false)
+//    private LocalDateTime localDateTime;
+//
+//    @Column(name = "state", nullable = false)
+//    private int state;
+//
+//    public Attendance(){
+//        this.localDateTime = LocalDateTime.now();
+//    }
+//    public Attendance(User user, int state) {
+//        this.user = user;
+//        this.localDateTime = LocalDateTime.now();
+//        this.state = state;
+//    }
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public User getUser() {
+//        return user;
+//    }
+//
+//    public void setUser(User user) {
+//        this.user = user;
+//    }
+//
+//    public LocalDateTime getLocalDateTime() {
+//        return localDateTime;
+//    }
+//
+//    public void setLocalDateTime(LocalDateTime localDateTime) {
+//        this.localDateTime = localDateTime;
+//    }
+//
+//    public int getState() {
+//        return state;
+//    }
+//
+//    public void setState(int state) {
+//        this.state = state;
+//    }
+//}
--- src/main/java/com/example/demo/service/AttendanceService.java
+++ src/main/java/com/example/demo/service/AttendanceService.java
@@ -1,22 +1,22 @@
-package com.example.demo.service;
-
-import com.example.demo.model.Attendance;
-
-import java.util.List;
-
-public interface AttendanceService {
-    List<Attendance> getAllLastYear();
-    List<Attendance> getAllLastMonth();
-    List<Attendance> getAllLastWeek();
-    List<Attendance> getAllToday();
-    List<Attendance> getLastYear();
-    List<Attendance> getLastMonth();
-    List<Attendance> getLastWeek();
-    List<Attendance> getToday();
-    List<Attendance> getByUser(Long userId);
-    List<Attendance> getByGroup(Long groupId);
-    List<Attendance> getByCourse(Long courseId);
-    List<Attendance> createAttendance(List<Attendance> l);
-    List<Attendance> updateAttendance(List<Attendance> l);
-
-}
+//package com.example.demo.service;
+//
+//import com.example.demo.model.Attendance;
+//
+//import java.util.List;
+//
+//public interface AttendanceService {
+//    List<Attendance> getAllLastYear();
+//    List<Attendance> getAllLastMonth();
+//    List<Attendance> getAllLastWeek();
+//    List<Attendance> getAllToday();
+//    List<Attendance> getLastYear();
+//    List<Attendance> getLastMonth();
+//    List<Attendance> getLastWeek();
+//    List<Attendance> getToday();
+//    List<Attendance> getByUser(Long userId);
+//    List<Attendance> getByGroup(Long groupId);
+//    List<Attendance> getByCourse(Long courseId);
+//    List<Attendance> createAttendance(List<Attendance> l);
+//    List<Attendance> updateAttendance(List<Attendance> l);
+//
+//}
